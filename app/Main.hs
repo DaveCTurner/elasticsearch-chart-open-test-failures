60bb03f92e42dcc6f9769d069317994706bf847d
David Turner <david.turner@elastic.co>
2026-03-29 16:25:24 +0100

    Adapt layout to rendered label sizes

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections #-}
 
 module Main (main) where
 
@@ -120,6 +121,56 @@ parseResultsPageNum name = do
   guard $ suffix == ".json" && not (null digits)
   readMaybe digits
 
+data PangoLabel = PangoLabel
+  { _pangoLabelLayout           :: PangoLayout
+  , _pangoLabelInkRectangle     :: PangoRectangle
+  , _pangoLabelLogicalRectangle :: PangoRectangle
+  }
+
+pangoRectangleWidth :: PangoRectangle -> Double
+pangoRectangleWidth (PangoRectangle x0 _ x1 _) = x1 - x0
+
+pangoRectangleHeight :: PangoRectangle -> Double
+pangoRectangleHeight (PangoRectangle _ y0 _ y1) = y1 - y0
+
+roundPixel :: Double -> Double
+roundPixel v = fromIntegral (ceiling v :: Int)
+
+pangoLabelHeight :: PangoLabel -> Double
+pangoLabelHeight PangoLabel{..} = roundPixel $ pangoRectangleHeight _pangoLabelLogicalRectangle
+
+pangoLabelWidth:: PangoLabel -> Double
+pangoLabelWidth PangoLabel{..} = roundPixel $ pangoRectangleWidth _pangoLabelLogicalRectangle
+
+pangoLabelInkXOffset :: PangoLabel -> Double
+pangoLabelInkXOffset PangoLabel {_pangoLabelLogicalRectangle = PangoRectangle lx0 _ _ _, _pangoLabelInkRectangle = PangoRectangle ix0 _ _ _} = ix0 - lx0
+
+pangoLabelInkYOffset :: PangoLabel -> Double
+pangoLabelInkYOffset PangoLabel {_pangoLabelLogicalRectangle = PangoRectangle _ ly0 _ _, _pangoLabelInkRectangle = PangoRectangle _ iy0 _ _} = iy0 - ly0
+
+renderPangoLabelTopCentre :: PangoLabel -> Double -> Double -> Render ()
+renderPangoLabelTopCentre l@PangoLabel{..} xCentre yTop = do
+  moveTo (roundPixel (xCentre - (pangoRectangleWidth _pangoLabelInkRectangle / 2) - pangoLabelInkXOffset l)) (roundPixel yTop)
+  showLayout _pangoLabelLayout
+
+renderPangoLabelTopLeft :: PangoLabel -> Double -> Double -> Render ()
+renderPangoLabelTopLeft PangoLabel{..} xLeft yTop = do
+  moveTo (roundPixel xLeft) (roundPixel yTop)
+  showLayout _pangoLabelLayout
+
+renderPangoLabelMiddleRight :: PangoLabel -> Double -> Double -> Render ()
+renderPangoLabelMiddleRight l@PangoLabel{..} xRight yMiddle = do
+  moveTo (roundPixel $ xRight - pangoRectangleWidth _pangoLabelInkRectangle - pangoLabelInkXOffset l)
+         (roundPixel $ yMiddle - (pangoRectangleHeight _pangoLabelInkRectangle / 2) - pangoLabelInkYOffset l)
+  showLayout _pangoLabelLayout
+
+data TeamData = TeamData
+  { _teamDataName       :: T.Text
+  , _teamDataIssueAges  :: M.Map Int [Double]
+  , _teamDataIssueCount :: Int
+  , _teamDataNameLabel  :: PangoLabel
+  }
+
 main :: IO ()
 main = do
   now <- getCurrentTime
@@ -136,7 +187,7 @@ main = do
           getResultsPages pageNum url = do
             when (pageNum > (10::Int)) $ threadDelay 1000000
             when (pageNum > (15::Int)) $ threadDelay 4000000
-            searchResponse <- getWith _opts url
+            searchResponse <- getWith opts url
             withFile ("results-page-" ++ show pageNum ++ ".json") WriteMode $ \h -> BL.hPutStr h $ searchResponse ^. responseBody
             case searchResponse ^? responseLink "rel" "next" . linkURL of
               Just url' -> getResultsPages (pageNum + 1) $ T.unpack $ T.decodeUtf8 url'
@@ -164,10 +215,11 @@ main = do
   issueTimesByTeamAndRisk <- M.map (M.map (reverse . sort)) <$> foldM
     ( \acc pageNum -> do
         mSr <- decodeFileStrict $ "results-page-" ++ show pageNum ++ ".json"
-        pure $ case mSr of
+        case mSr of
           Nothing -> error $ "failed to decode page " ++ show pageNum
-          Just (SearchResponseBody issues) ->
-            M.unionsWith (M.unionWith (++)) (
+          Just (SearchResponseBody issues) -> do
+            forM_ issues $ \issue -> when (issueTeamNames issue == ["Missing"]) $ print issue
+            return $ M.unionsWith (M.unionWith (++)) (
               acc : [ M.singleton teamName (M.singleton riskLevel [issueAgeDays now issue])
                     | issue     <- issues
                     , riskLevel <- [1 .. issueRiskLevel issue]
@@ -179,9 +231,23 @@ main = do
 
   pangoContext <- cairoCreateContext . Just =<< cairoFontMapGetDefault
   titleFontDescription <- fontDescriptionFromString ("Sans Bold" :: String)
-  fontDescriptionSetSize titleFontDescription 12.0
+  fontDescriptionSetSize titleFontDescription 16.0
+  teamNameFontDescription <- fontDescriptionFromString ("Sans Bold" :: String)
+  fontDescriptionSetSize teamNameFontDescription 12.0
   labelFontDescription <- fontDescriptionFromString ("Sans" :: String)
   fontDescriptionSetSize labelFontDescription 8.0
+
+  let pangoLabel :: FontDescription -> String -> IO PangoLabel
+      pangoLabel font text = do
+        _pangoLabelLayout <- layoutText pangoContext text
+        layoutSetFontDescription _pangoLabelLayout $ Just font
+        (_pangoLabelInkRectangle, _pangoLabelLogicalRectangle) <- layoutGetExtents _pangoLabelLayout
+        return $ PangoLabel{..}
+
+      pangoAxisLabel  = pangoLabel labelFontDescription
+
+  titleLabel <- pangoLabel titleFontDescription $ "Cumulative open test failures by age, per team and risk level - " ++ show (utctDay now)
+  ageAxisLabel <- pangoAxisLabel "Age (weeks)"
 
   let teamCount :: Int
       teamCount  = M.size issueTimesByTeamAndRisk
@@ -191,15 +257,6 @@ main = do
         [ M.foldr (\i n -> max n $ length i) 11 m
         | m <- M.elems issueTimesByTeamAndRisk
         ]
-
-      titleHeight :: Double
-      titleHeight = 30.0
-
-      teamNameHeight :: Double
-      teamNameHeight = 25.0
-
-      axisLabelHeight :: Double
-      axisLabelHeight = 25.0
 
       issueHeight :: Double
       issueHeight = 5.0
@@ -213,32 +270,49 @@ main = do
       margin :: Double
       margin = 30.0
 
-      imageWidth, imageHeight :: Int
-      imageWidth  = ceiling $ margin * 2.0 + dayCount * dayWidth
-      imageHeight = ceiling $ margin * 2.0 + fromIntegral teamCount * (teamNameHeight + axisLabelHeight) + fromIntegral issueCount * issueHeight + titleHeight
+  weekAgeLabels <- forM [0,7..floor dayCount] $ \vGrid -> (vGrid,) <$> pangoAxisLabel (show $ vGrid `div` (7::Int))
 
-      renderTeams :: [(T.Text, M.Map Int [Double])] -> Render ()
-      renderTeams [] = return ()
-      renderTeams ((teamName, issueCounts):ts) = renderTeam teamName issueCounts >> renderTeams ts
+  teamData <- forM (M.toList issueTimesByTeamAndRisk) $ \(_teamDataName, _teamDataIssueAges) -> do
+    let _teamDataIssueCount = M.foldr (\i n -> max n $ length i) 11 _teamDataIssueAges
+    _teamDataNameLabel <- pangoLabel teamNameFontDescription $ T.unpack _teamDataName
+    return TeamData{..}
+
+  issueCountLabels <- forM [0,10..maximum $ map _teamDataIssueCount teamData] $ \hGrid -> pangoAxisLabel (show hGrid)
+
+  let titleHeight :: Double
+      titleHeight = pangoLabelHeight titleLabel
+
+      teamNameHeight :: Double
+      teamNameHeight = maximum $ map (pangoLabelHeight . _teamDataNameLabel) teamData
+
+      axisMarkHeight :: Double
+      axisMarkHeight = maximum $ map (pangoLabelHeight . snd) weekAgeLabels
+
+      axisLabelHeight :: Double
+      axisLabelHeight = pangoLabelHeight ageAxisLabel
+
+      axisLabelVSpace :: Double
+      axisLabelVSpace = 5.0
+
+      axisMarkWidth = maximum $ map pangoLabelWidth issueCountLabels
+
+      imageWidth, imageHeight :: Int
+      imageWidth  = ceiling $ margin * 2.0 + dayCount * dayWidth + axisMarkWidth
+      imageHeight = ceiling $ margin * 2.0 + fromIntegral teamCount * (teamNameHeight + axisMarkHeight + axisLabelHeight + axisLabelVSpace*2) + fromIntegral issueCount * issueHeight + titleHeight
 
       setColor :: Int -> Int -> Int -> Render ()
       setColor rr gg bb = setSourceRGB (fromIntegral rr / 255.0) (fromIntegral gg / 255.0) (fromIntegral bb / 255.0)
 
-      roundPixel :: Double -> Double
-      roundPixel v = fromIntegral (floor v :: Int)
-
-      renderTeam :: T.Text -> M.Map Int [Double] -> Render ()
-      renderTeam teamName issueCounts = do
+      renderTeam :: TeamData -> Render ()
+      renderTeam TeamData{..} = do
         do
           setSourceRGB 0.0 0.0 0.0
-          pangoLayout <- createLayout (T.unpack teamName)
-          liftIO $ layoutSetFontDescription pangoLayout $ Just labelFontDescription
-          PangoRectangle _x0 y0 _x1 y1 <- liftIO $ fst <$> layoutGetExtents pangoLayout
-          moveTo 5 (roundPixel $ teamNameHeight - (y1 - y0) - 5)
-          showLayout pangoLayout
+          renderPangoLabelTopLeft _teamDataNameLabel 5 0
         translate 0 teamNameHeight
 
-        let teamIssueCount = M.foldr (\i n -> max n $ length i) 11 issueCounts
+        let issueCounts = _teamDataIssueAges
+        let teamIssueCount = _teamDataIssueCount
+
         translate 0 (fromIntegral teamIssueCount * issueHeight)
 
         forM_ (M.toList issueCounts) $ \(riskLevel, issueAges) -> do
@@ -254,7 +328,7 @@ main = do
           lineTo 0 (roundPixel (negate $ fromIntegral (length issueAges) * issueHeight))
           fill
 
-        forM_ [0,10..teamIssueCount] $ \hGrid -> do
+        forM_ (zip [0,10..teamIssueCount] issueCountLabels) $ \(hGrid, issueCountLabel) -> do
           let yy = (+0.5) $ roundPixel $ negate $ fromIntegral hGrid * issueHeight
           setLineWidth 1.0
           setSourceRGBA 0.0 0.0 0.0 0.2
@@ -263,13 +337,9 @@ main = do
           stroke
 
           setSourceRGB 0.0 0.0 0.0
-          pangoLayout <- createLayout (show hGrid)
-          liftIO $ layoutSetFontDescription pangoLayout $ Just labelFontDescription
-          PangoRectangle _ y0 x1 y1 <- liftIO $ snd <$> layoutGetExtents pangoLayout
-          moveTo (roundPixel $ negate $ x1+8) (roundPixel $ yy - (y1-y0)/2 )
-          showLayout pangoLayout
+          renderPangoLabelMiddleRight issueCountLabel (-5) (yy-1)
 
-        forM_ [0,7..floor dayCount] $ \vGrid -> do
+        forM_ weekAgeLabels $ \(vGrid, weekAgeLabel) -> do
           let xx = (+0.5) $ roundPixel $ fromIntegral (vGrid::Int) * dayWidth
           setLineWidth 1.0
           setSourceRGBA 0.0 0.0 0.0 0.2
@@ -278,18 +348,7 @@ main = do
           stroke
 
           setSourceRGB 0.0 0.0 0.0
-          pangoLayout <- createLayout (show (div vGrid 7))
-          liftIO $ layoutSetFontDescription pangoLayout $ Just labelFontDescription
-          PangoRectangle x0 _ x1 y1 <- liftIO $ fst <$> layoutGetExtents pangoLayout
-          moveTo (roundPixel $ xx - (x1-x0)/2) (roundPixel $ y1+1)
-          showLayout pangoLayout
-
-        do
-          pangoLayout <- createLayout ("age (weeks)"::String)
-          liftIO $ layoutSetFontDescription pangoLayout $ Just labelFontDescription
-          PangoRectangle x0 _ x1 y1 <- liftIO $ fst <$> layoutGetExtents pangoLayout
-          moveTo (roundPixel $ (dayCount * dayWidth - (x1-x0))/2) (roundPixel $ y1+15)
-          showLayout pangoLayout
+          renderPangoLabelTopCentre weekAgeLabel xx axisLabelVSpace
 
         setSourceRGB 0.0 0.0 0.0
         setLineWidth 2.0
@@ -298,23 +357,21 @@ main = do
         lineTo (dayCount * dayWidth) 0
         stroke
 
-        translate 0 axisLabelHeight
+        translate 0 (axisMarkHeight + axisLabelVSpace)
+        renderPangoLabelTopCentre ageAxisLabel (dayCount * dayWidth / 2) axisLabelVSpace
+        translate 0 (axisLabelHeight + axisLabelVSpace)
 
   withImageSurface FormatARGB32 imageWidth imageHeight $ \surface -> do
     renderWith surface $ do
       setSourceRGB 1.0 1.0 1.0
       rectangle 0 0 (fromIntegral imageWidth) (fromIntegral imageHeight)
       fill
-      translate margin margin
+      translate (margin + axisMarkWidth) margin
       do
         setSourceRGB 0.0 0.0 0.0
-        pangoLayout <- createLayout ("Cumulative open test failures by age, per team and risk level - " ++ show (utctDay now))
-        liftIO $ layoutSetFontDescription pangoLayout $ Just titleFontDescription
-        PangoRectangle x0 y0 x1 y1 <- liftIO $ fst <$> layoutGetExtents pangoLayout
-        moveTo (roundPixel $ (fromIntegral imageWidth - (x1-x0))/2) (roundPixel $ (titleHeight - (y1-y0))/2)
-        showLayout pangoLayout
+        renderPangoLabelTopCentre titleLabel (fromIntegral imageWidth / 2) 0
         translate 0 titleHeight
-      renderTeams (M.toList issueTimesByTeamAndRisk)
+      mapM_ renderTeam teamData
     surfaceWriteToPNG surface "test-failures.png"
 
   do
@@ -322,8 +379,7 @@ main = do
     fontDescription <- fontDescriptionFromString ("Sans Bold" :: String)
     fontDescriptionSetSize fontDescription 120.0
     layoutSetFontDescription helloLayout $ Just fontDescription
-    extents@(PangoRectangle ix0 iy0 ix1 iy1, PangoRectangle lx0 ly0 lx1 ly1) <- layoutGetExtents helloLayout
-    print extents
+    (PangoRectangle ix0 iy0 ix1 iy1, PangoRectangle lx0 ly0 lx1 ly1) <- layoutGetExtents helloLayout
     withImageSurface FormatARGB32 imageWidth imageHeight $ \surface -> do
       renderWith surface $ do
         setSourceRGB 0 0 0
